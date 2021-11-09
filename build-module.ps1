f0a29c2e56deb9c40365e6857903d440e8df3155
ShortArrow <bamboogeneral@live.jp>
2021-11-09 17:18:18 +0900

    change hierarchy for builder

---
+++ build-module.ps1
@@ -0,0 +1,33 @@
+function Build-Vba {
+    param (
+        [string]$sourceFilename = "./main.ps1",
+        [string]$temporaryFilename = "\temp.ps1"
+    )
+    
+    [string[]]$codeList = Get-Content $sourceFilename
+    [string]$Code = ""
+    $codelistBeforePrint = @(
+        "Static Sub CreatePayload()"
+        "`tDim s"
+        "`tDim n"
+        "`ts = Environ(`"TEMP`") + `"" + $temporaryFilename + "`""
+        "`tn = FreeFile"
+        "`tOpen s For Output As #n"
+    )
+    $codelistAfterPrint = @(
+        "`tClose #n"
+        "End Sub"
+    )
+
+    for ($i = 0; $i -lt $codeList.Count; $i++) {
+        $codeList[$i] = ("`tPrint #n, `"" + ($codeList[$i] -replace "`"", "`"`"") + "`"")
+    }
+        
+    $codeList = $codelistBeforePrint + $codeList + $codelistAfterPrint
+        
+    foreach ($item in $codelist) {
+        $Code += $item + "`n"
+    }
+    $Code | Out-File -FilePath "src/Classes/PayloadCreater.vb"
+}
+Build-Vba -sourceFilename "./src/ps/main.ps1" -temporaryFilename "\temp.ps1"
